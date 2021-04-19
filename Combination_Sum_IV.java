63a25bacd10ce302c655c3438a7ee234b6fb4471
Jahnvi <Janvi.2678@gmail.com>
2021-04-19 13:19:22 +0530

    LeetCode: Medium (DP)
    https://leetcode.com/problems/combination-sum-iv/

---
+++ Combination_Sum_IV.java
@@ -0,0 +1,52 @@
+/*  Given an array of distinct integers nums and a target integer target, 
+return the number of possible combinations that add up to target.
+The answer is guaranteed to fit in a 32-bit integer.
+
+ Example 1:
+Input: nums = [1,2,3], target = 4
+Output: 7
+Explanation:
+The possible combination ways are:
+(1, 1, 1, 1)
+(1, 1, 2)
+(1, 2, 1)
+(1, 3)
+(2, 1, 1)
+(2, 2)
+(3, 1)
+Note that different sequences are counted as different combinations.
+
+Example 2:
+Input: nums = [9], target = 3
+Output: 0
+ 
+Constraints:
+1 <= nums.length <= 200
+1 <= nums[i] <= 1000
+All the elements of nums are unique.
+1 <= target <= 1000                           */
+___________________________________________________________________________________________________________________________________
+class Main {
+     int count = 0;
+    public int combinationSum4(int[] nums, int target) {
+        
+        if(nums == null || nums.length < 1)return 0;
+        int[] dp = new int[target+1];
+        //dp[0] = 0;  
+        for(int i=1;i<=target;i++){
+            
+            for(int j=0;j<nums.length;j++){
+                
+              if(i > nums[j]){
+                  dp[i] += dp[i-nums[j]];
+              }
+                else if(i == nums[j]){
+                    dp[i] += 1;
+                }
+                
+            }
+        }
+        
+        return dp[target];
+    }
+}
