58e9df59165314840a4c948fb8c3fba65a193056
Jahnvi <Janvi.2678@gmail.com>
2021-07-05 19:20:38 +0530

    LeetCode

--- Count the Number of Consistent Strings.java
+++ Count the Number of Consistent Strings.java
@@ -1,7 +1,7 @@
 /*   You are given a string allowed consisting of distinct characters and an array of strings words.
 A string is consistent if all characters in the string appear in the string allowed.
 Return the number of consistent strings in the array words.
-
+ 
 Example 1:
 Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
 Output: 2
