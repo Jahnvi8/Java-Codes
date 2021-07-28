e6b0601f32810f5a5a6821e117dca998fde06bf9
Jahnvi <Janvi.2678@gmail.com>
2021-07-28 14:09:28 +0530

    LeetCode

---
+++ Gray Code.java
@@ -0,0 +1,67 @@
+// An n-bit gray code sequence is a sequence of 2n integers where:
+
+// Every integer is in the inclusive range [0, 2n - 1],
+// The first integer is 0,
+// An integer appears no more than once in the sequence,
+// The binary representation of every pair of adjacent integers differs by exactly one bit, and
+// The binary representation of the first and last integers differs by exactly one bit.
+// Given an integer n, return any valid n-bit gray code sequence.
+
+ 
+
+// Example 1:
+
+// Input: n = 2
+// Output: [0,1,3,2]
+// Explanation:
+// The binary representation of [0,1,3,2] is [00,01,11,10].
+// - 00 and 01 differ by one bit
+// - 01 and 11 differ by one bit
+// - 11 and 10 differ by one bit
+// - 10 and 00 differ by one bit
+// [0,2,3,1] is also a valid gray code sequence, whose binary representation is [00,10,11,01].
+// - 00 and 10 differ by one bit
+// - 10 and 11 differ by one bit
+// - 11 and 01 differ by one bit
+// - 01 and 00 differ by one bit
+// Example 2:
+
+// Input: n = 1
+// Output: [0,1]
+ 
+
+// Constraints:
+
+// 1 <= n <= 16
+_________________________________________________________________________________________________________________________________________
+class Solution {
+    public List<String> helper(int n) {
+        if(n == 1){
+            List<String> one = new ArrayList();
+            one.add("0");
+            one.add("1");
+            return one;
+        }
+        List<String> result = helper(n-1);
+        List<String> temp = new ArrayList();
+        
+        for(int i = 0 ;i< result.size();i++){
+            String cur ="0" + result.get(i);
+            temp.add(cur);
+        }
+        for(int i = result.size()-1;i>=0;i--){
+            String cur =  "1" + result.get(i);
+            temp.add(cur);
+        }
+        return temp;
+    }
+    public List<Integer> grayCode(int n) {
+        List<String> l =  helper(n);
+        List<Integer> result = new ArrayList();
+        
+        for(String i : l){
+            result.add(Integer.parseInt(i,2));
+        }
+        return result;
+    }
+}
