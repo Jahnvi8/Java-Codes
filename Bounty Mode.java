4dfb4828dd836b873d850a1b35d5164a8461d3c3
Jahnvi <Janvi.2678@gmail.com>
2021-07-30 00:09:16 +0530

    CodeChef

--- Bounty Mode.java
+++ Bounty Mode.java
@@ -30,7 +30,7 @@ import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
 class Codechef
-{
+{ 
 	public static void main (String[] args) throws java.lang.Exception
 	{
 	   try {
