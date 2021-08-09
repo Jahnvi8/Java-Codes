a53d5cbc8e1d733609edc928ebafa8caae13d1a8
Jahnvi <Janvi.2678@gmail.com>
2021-08-09 21:42:28 +0530

    HackerEarth

---
+++ Minimum AND xor OR.java
@@ -0,0 +1,178 @@
+// Problem
+// You are given an array  of  integers. Determine the minimum value of the following expression for all valid :
+
+// , where .
+
+// Input format
+
+// First line: A single integer  denoting the number of test cases
+// For each test case:
+// First line contains a single integer , denoting the size of the array
+// Second line contains  space-separated integers 
+// Output format
+
+// For each test case, print a single line containing one integer that represents the minimum value of the given expression
+
+// Constraints
+
+
+// Note: Sum of  overall test cases does not exceed 
+
+// Sample Input
+// 2
+// 5
+// 1 2 3 4 5
+// 3
+// 2 4 7
+
+// Sample Output
+// 1
+// 3
+
+// Time Limit: 1
+// Memory Limit: 256
+// Source Limit:
+// Explanation
+// For test case #1, we can select elements  and , the value of the expression is , which is the minimum possible value. Another possible pair is 4 and 5.
+
+// For test case #2, we can select elements 4 and 7, the value of the expression is , which is the minimum possible value.
+___________________________________________________________________________________________________________________________________________
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+ 
+ class TestClass  {
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+ 
+        public Reader()
+        {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+ 
+        public Reader(String file_name) throws IOException
+        {
+            din = new DataInputStream(
+                new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+ 
+        public String readLine() throws IOException
+        {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    if (cnt != 0) {
+                        break;
+                    }
+                    else {
+                        continue;
+                    }
+                }
+                buf[cnt++] = (byte)c;
+            }
+            return new String(buf, 0, cnt);
+        }
+ 
+        public int nextInt() throws IOException
+        {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+ 
+            if (neg)
+                return -ret;
+            return ret;
+        }
+ 
+        public long nextLong() throws IOException
+        {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+ 
+        public double nextDouble() throws IOException
+        {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+ 
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+ 
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+ 
+            if (neg)
+                return -ret;
+            return ret;
+        }
+ 
+        private void fillBuffer() throws IOException
+        {
+            bytesRead = din.read(buffer, bufferPointer = 0,
+                                 BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+ 
+        private byte read() throws IOException
+        {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+ 
+        public void close() throws IOException
+        {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+ 
+    public static void main(String args[] ) throws Exception {
+      
+        Reader s = new Reader();
+        int t = s.nextInt();
+     
+
+    }
+}
