61975dee20ad05c2a48128ce86fcd68558cdb3e1
Jahnvi <Janvi.2678@gmail.com>
2021-08-18 23:09:34 +0530

    HackerEarth

---
+++ Stack and Queue <Nissan>.java
@@ -0,0 +1,188 @@
+// Problem
+// You are given a stack of N integers such that the first element represents the top of the stack and the last element represents the bottom of the stack. You need to pop at least one element from the stack. At any one moment, you can convert stack into a queue. The bottom of the stack represents the front of the queue. You cannot convert the queue back into a stack. Your task is to remove exactly K elements such that the sum of the K removed elements is maximised.
+
+// Input format :     
+
+// The first line consists of two space-separated integers N and K.
+// The second line consists of N space-separated integers denoting the elements of the stack.
+// Output format :
+
+// Print the maximum possible sum of the K removed elements
+// Constraints :
+
+// Sample Input
+// 10 5
+// 10 9 1 2 3 4 5 6 7 8
+// Sample Output
+// 40
+// Time Limit: 1
+// Memory Limit: 256
+// Source Limit:
+// Explanation
+// Pop two elements from the stack. i.e {10,9}
+
+// Then convert the stack into queue and remove first three elements from the queue. i.e {8,7,6}.
+
+// The maximum possible sum is 10+9+8+7+6 = 40
+___________________________________________________________________________________________________________________________________
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.*;
+import java.util.StringTokenizer;
+ 
+ class TestClass  {
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+ 
+        public Reader()
+        {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+ 
+        public Reader(String file_name) throws IOException
+        {
+            din = new DataInputStream(
+                new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+ 
+        public String readLine() throws IOException
+        {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    if (cnt != 0) {
+                        break;
+                    }
+                    else {
+                        continue;
+                    }
+                }
+                buf[cnt++] = (byte)c;
+            }
+            return new String(buf, 0, cnt);
+        }
+ 
+        public int nextInt() throws IOException
+        {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+ 
+            if (neg)
+                return -ret;
+            return ret;
+        }
+ 
+        public long nextLong() throws IOException
+        {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+ 
+        public double nextDouble() throws IOException
+        {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+ 
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+ 
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+ 
+            if (neg)
+                return -ret;
+            return ret;
+        }
+ 
+        private void fillBuffer() throws IOException
+        {
+            bytesRead = din.read(buffer, bufferPointer = 0,
+                                 BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+ 
+        private byte read() throws IOException
+        {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+ 
+        public void close() throws IOException
+        {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+ 
+    public static void main(String args[] ) throws Exception {
+      
+        Reader sc = new Reader();
+        int n,k;
+        n = sc.nextInt();
+        k = sc.nextInt();
+ 
+        long[] arr = new long[n];
+        long sum = 0, sumK = 0;
+ 
+        for(int i = 0; i < n; i++){
+            long ele = sc.nextLong();
+            arr[i] = ele;
+            if(i >= n-k) sumK += arr[i];
+        }
+ 
+        long max = Integer.MIN_VALUE;
+        for(int i = 0; i < k; i++){
+            sum += arr[i];
+            sumK -= arr[n-k+i];
+            max = Math.max(max,sum+sumK);
+        }
+        System.out.println(max);
+        
+ 
+        }
+ 
+    
+}
