ad3480221e74d2e4212b5a9b1c1810e096f55625
Jahnvi <Janvi.2678@gmail.com>
2021-08-25 20:04:32 +0530

    GFG

--- Find Missing And Repeating.java
+++ Find Missing And Repeating.java
@@ -1,6 +1,6 @@
 // Given an unsorted array Arr of size N of positive integers. One number 'A' from set {1, 2, …N} is missing and one number 'B' occurs twice in array. Find these two numbers.
 
-// Example 1:
+// Example 1: 
 
 // Input:
 // N = 2
