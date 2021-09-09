60c43c26cae335baf296611ad43a0b9a0f10fb5f
Jahnvi <Janvi.2678@gmail.com>
2021-09-09 17:12:13 +0530

    GFG

--- Count ways to reach the n'th stair.java
+++ Count ways to reach the n'th stair.java
@@ -1,4 +1,5 @@
-// There are n stairs, a person standing at the bottom wants to reach the top. The person can climb either 1 stair or 2 stairs at a time. Count the number of ways, the person can reach the top (order does matter).
+// There are n stairs, a person standing at the bottom wants to reach the top. The person can climb either 1 stair or 2 stairs at a time. Count the number of ways, 
+// the person can reach the top (order does matter).
 
 // Example 1:
 
