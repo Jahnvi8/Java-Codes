9a479ed773c0d7e20a36a391afe9760ac36dde4d
Jahnvi <Janvi.2678@gmail.com>
2021-10-06 18:20:37 +0530

    CodeForces

---
+++ Anton and Letters.java
@@ -0,0 +1,34 @@
+// Recently, Anton has found a set. The set consists of small English letters. Anton carefully wrote out all the letters from the set in one line, separated by a comma. He also added an opening curved bracket at the beginning of the line and a closing curved bracket at the end of the line.
+
+// Unfortunately, from time to time Anton would forget writing some letter and write it again. He asks you to count the total number of distinct letters in his set.
+
+// Input
+// The first and the single line contains the set of letters. The length of the line doesn't exceed 1000. It is guaranteed that the line starts from an opening curved bracket and ends with a closing curved bracket. Between them, small English letters are listed, separated by a comma.
+// Each comma is followed by a space.
+
+// Output
+// Print a single number — the number of distinct letters in Anton's set.
+
+// Examples
+// inputCopy
+// {a, b, c}
+// outputCopy
+// 3
+// inputCopy
+// {b, a, b, a}
+// outputCopy
+// 2
+// inputCopy
+// {}
+// outputCopy
+// 0
+
+_____________________________________________________________________________________________________________________________________________________________________________________________________
+import java.util.*;
+public class Sol {
+
+	public static void main(String[] args) {
+		System.out.println(new Scanner(System.in).nextLine().replaceAll("[{}, ]","").chars(). distinct().count());
+	}
+
+}
