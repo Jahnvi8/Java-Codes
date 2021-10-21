2c518ec7013a353cf3d214824fee7398a98008e8
Jahnvi <Janvi.2678@gmail.com>
2021-10-21 22:37:51 +0530

    CodeForces

--- Adjacency Hatred.java
+++ Adjacency Hatred.java
@@ -37,7 +37,7 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
-	public static void main (String[] args) throws java.lang.Exception
+	public static void main (String[] args) throws java.lang.Exception 
 	{
 	    try {
 	           Scanner sc=new Scanner(System.in);
