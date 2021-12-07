2a4db3362e27f225e8ae82a7d6b6b5b20f646002
Jahnvi <Janvi.2678@gmail.com>
2021-12-07 12:32:03 +0530

    LeetCode

---
+++ Max Area of Island.java
@@ -0,0 +1,54 @@
+// You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) 
+//   You may assume all four edges of the grid are surrounded by water.
+
+// The area of an island is the number of cells with a value 1 in the island.
+
+// Return the maximum area of an island in grid. If there is no island, return 0.
+
+ 
+
+// Example 1:
+
+
+// Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],
+//                [0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+// Output: 6
+// Explanation: The answer is not 11, because the island must be connected 4-directionally.
+// Example 2:
+
+// Input: grid = [[0,0,0,0,0,0,0,0]]
+// Output: 0
+ 
+
+// Constraints:
+
+// m == grid.length
+// n == grid[i].length
+// 1 <= m, n <= 50
+// grid[i][j] is either 0 or 1.
+______________________________________________________________________________________________________________________________________________________________________
+class Solution {
+    int[][] grid;
+    boolean[][] seen;
+
+    public int area(int r, int c) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length ||
+                seen[r][c] || grid[r][c] == 0)
+            return 0;
+        seen[r][c] = true;
+        return (1 + area(r+1, c) + area(r-1, c)
+                  + area(r, c-1) + area(r, c+1));
+    }
+
+    public int maxAreaOfIsland(int[][] grid) {
+        this.grid = grid;
+        seen = new boolean[grid.length][grid[0].length];
+        int ans = 0;
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                ans = Math.max(ans, area(r, c));
+            }
+        }
+        return ans;
+    }
+}
