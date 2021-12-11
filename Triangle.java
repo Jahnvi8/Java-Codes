254e934157bb6b1f0668ce59959d0e7dc1b7d775
Jahnvi <Janvi.2678@gmail.com>
2021-12-11 13:14:14 +0530

    LeetCode

---
+++ Triangle.java
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/triangle/
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        for(int i=triangle.size()-2;i>= 0;i--) {
+            for(int j=0;j<triangle.get(i).size();j++) {
+                int min=Math.min(triangle.get(i+1).get(j),triangle.get(i+1).get(j+1));
+                int sum=min+triangle.get(i).get(j);
+                triangle.get(i).set(j, sum);
+            }
+        }
+        return triangle.get(0).get(0);
+    }
+}
