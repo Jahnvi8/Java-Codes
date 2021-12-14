800a662a6f3d4bd57fc8301093be473abf6f00c0
Jahnvi <Janvi.2678@gmail.com>
2021-12-14 16:45:38 +0530

    LeetCode

---
+++ Find Peak Element.java
@@ -0,0 +1,16 @@
+https://leetcode.com/problems/find-peak-element/
+
+
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1])
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+}
